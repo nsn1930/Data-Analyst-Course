d195987c5b55a8262154cceb00f87f48964f4efe
nsn1930 <94860531+nsn1930@users.noreply.github.com>
2024-06-11 11:04:22 +0300

    Delete CAR SPR1 (Visualising Data Using Power BI) directory

--- CAR SPR1 (Visualising Data Using Power BI)/PowerBIProject_SQL_Queries_Pavel_Brodovskij_DA/CAR SPR1 AVGOrderValue.sql
+++
@@ -1,7 +0,0 @@
-SELECT
-  ROUND(SUM(salesorderheader.TotalDue)/COUNT(salesorderheader.orderdate),2) AS AVGOrderValue
-FROM
-  `adwentureworks_db.salesorderheader` AS salesorderheader
-WHERE
-  salesorderheader.OrderDate BETWEEN '2001-07-01'
-  AND '2004-06-30'
